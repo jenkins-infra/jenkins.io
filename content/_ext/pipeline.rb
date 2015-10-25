f4b43744035a17eacea3deeab94ee6fd6641eab1
R. Tyler Croy <tyler@monkeypox.org>
2015-10-25 08:54:59 -0700

    Add a blog post listing page under /node like the current site

--- content/_ext/pipeline.rb
+++ content/_ext/pipeline.rb
@@ -1,13 +1,19 @@
 require 'yearposts'
 
 Awestruct::Extensions::Pipeline.new do
+  # Register all our blog content under the `site.posts` variable
   extension YearPosts.new('/blog', :posts)
+
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Sitemap.new
   extension Awestruct::Extensions::DataDir.new
 
-  #extension Awestruct::Extensions::Tagger.new(:posts, 
+  #extension Awestruct::Extensions::Tagger.new(:posts,
   #                                             '/blog')
+
+  extension Awestruct::Extensions::Paginator.new(:posts,
+                                                  '/node/index',
+                                                  :per_page=> 8)
 
   extension Awestruct::Extensions::Atomizer.new(:posts,
                                                 '/rss.xml',
