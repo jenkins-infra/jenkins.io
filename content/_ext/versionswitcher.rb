d16f72ce2bdfad8677efad623e9eab511e8e84bf
R. Tyler Croy <tyler@monkeypox.org>
2015-12-28 14:15:22 -0800

    When operating in legacy mode, legacy pages should just redirect to home
    Fixes WEBSITE-43

--- content/_ext/versionswitcher.rb
+++ content/_ext/versionswitcher.rb
@@ -3,8 +3,16 @@
 # legacy and jenkins.io sites side-by-side without needing to convert expects that some files are in the _tmp directory so it can
 # add their content to the site object
 
+require 'legacy'
+
 class VersionSwitcher
+  include Legacy
+
   def transform(site, page, rendered)
+    if legacy? && page.legacy == false
+      puts page.inspect
+      return '<html><head><meta http-equiv="refresh" content="0;/"/>'
+    end
     return rendered
   end
 end
