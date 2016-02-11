544cf1c410724fb251afef4e43cf4bae3de25c9a
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-02-11 12:34:44 -0600

    Log loading of solution pages

--- content/_ext/solutionpage.rb
+++ content/_ext/solutionpage.rb
@@ -7,10 +7,11 @@ require 'awestruct/page'
 class SolutionPage
   def execute(site)
     solutions = site.pages.select { |p| p.source_path =~ /solutions/ }
-
+    print "Loading solution pages\n"
     solutions.each do |solution|
       page = site.engine.load_page(solution.source_path)
       page.output_path = "/s/#{File.basename(solution.source_path, '.html.haml')}/index.html"
+      print " = Imported solution #{solution.source_path} as #{page.output_path}\n" 
       site.pages << page
     end
   end
