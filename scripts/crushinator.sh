d2a6185873fd243e202d790d5595459fc591902b
R. Tyler Croy <tyler@monkeypox.org>
2016-07-25 16:53:10 -0700

    pngcrush image assets which need crushing (#318)
    Also added a handy script for doing somewhat inline crushing in the future,
    invoked via:
    
        find . -iname "*.png" -exec ./scripts/crushinator.sh {} \;

---
+++ scripts/crushinator.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+FILENAME=$1
+OUTPUTFILE="${FILENAME}_pngcrush_`date '+%Y%m%d%H%M'`"
+
+if [ -z $FILENAME ]; then
+    echo "Need a filename as the first argument"
+    exit 1;
+fi;
+
+which pngcrush 2>&1 > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Must have pngcrush installed"
+    exit 1;
+fi;
+
+
+pngcrush -reduce $FILENAME $OUTPUTFILE
+
+if [ $? -ne 0 ]; then
+    echo "Error crushing ${FILENAME} to ${OUTPUTFILE}"
+    exit 1;
+fi;
+
+mv $OUTPUTFILE $FILENAME
