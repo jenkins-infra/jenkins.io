8f868c41c2e7f6214b3f21c31193f0162d520ec5
R. Tyler Croy <tyler@monkeypox.org>
2016-12-02 06:54:53 -0800

    Add the expand_link() helper to create absolute paths for rendered links
    This helps render the site correctly when it won't be hosted at the root URL of
    the domain.

--- content/_ext/activenav.rb
+++ content/_ext/activenav.rb
@@ -3,4 +3,8 @@ module ActiveNav
   def active_link?(section_name, options={:css_class => 'active'})
     return options[:css_class] if page.section == section_name
   end
+
+  def expand_link(relative_url)
+    return [site.base_url, relative_url].join('/')
+  end
 end
