568a85de49fb7f9fabd907cdfd95de8559aa86fe
R. Tyler Croy <tyler@monkeypox.org>
2017-01-25 11:06:55 -0800

    Add an active_href helper to make it easier to identify which page in the guided tour one is on

--- content/_ext/activenav.rb
+++ content/_ext/activenav.rb
@@ -7,4 +7,22 @@ module ActiveNav
   def expand_link(relative_url)
     return [site.base_url, relative_url].join('/')
   end
+
+  def active_href(relative_url, text, options={:class => nil})
+    classes = []
+    if options[:class]
+      classes << options[:class]
+    end
+
+    if page.output_path.match(relative_url)
+      classes << 'active'
+    end
+    classes = classes.join(' ')
+
+    return [
+      "<a href=\"#{expand_link(relative_url)}\" class=\"#{classes}\">",
+      text,
+      '</a>',
+        ].join('')
+  end
 end
