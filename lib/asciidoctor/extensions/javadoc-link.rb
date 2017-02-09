ecbe81ca75c34e086cd18182dcf19cc0c756c5a9
Daniel Beck <daniel-beck@users.noreply.github.com>
2017-02-09 15:06:13 +0100

    Rename javadoc: to jenkinsdoc:, add javadoc: and staplerdoc:

--- lib/asciidoctor/extensions/javadoc-link.rb
+++ lib/asciidoctor/extensions/javadoc-link.rb
@@ -3,16 +3,17 @@ require 'asciidoctor/extensions'
 
 #
 # Usage:
-# javadoc:SCM[]
-# javadoc:SCM[some label]
-# javadoc:hudson.scm.SCM[]
-# javadoc:hudson.scm.SCM[some label]
-# javadoc:hudson.scm.SCM#anchor[]
-# javadoc:hudson.scm.SCM#anchor[some label]
+# jenkinsdoc:SCM[]
+# jenkinsdoc:SCM[some label]
+# jenkinsdoc:hudson.scm.SCM[]
+# jenkinsdoc:hudson.scm.SCM[some label]
+# jenkinsdoc:hudson.scm.SCM#anchor[]
+# jenkinsdoc:hudson.scm.SCM#anchor[some label]
 #
+
 Asciidoctor::Extensions.register do
   inline_macro do
-    named :javadoc
+    named :jenkinsdoc
     name_positional_attributes 'label'
 
     process do |parent, target, attrs|
@@ -30,6 +31,49 @@ Asciidoctor::Extensions.register do
         label = (attrs.has_key? 'label') ? attrs['label'] : classname
         target = %(http://javadoc.jenkins.io/byShortName/#{classname}) 
       end
+
+      title = %(Javadoc for #{classname})
+
+      (create_anchor parent, label, type: :link, target: target, attributes: {'title' => title}).render
+    end
+  end
+end
+
+Asciidoctor::Extensions.register do
+  inline_macro do
+    named :staplerdoc
+    name_positional_attributes 'label'
+
+    process do |parent, target, attrs|
+
+      classname = target.gsub(/#.*/, '')
+      classurl = classname.gsub(/\./, '/') + ".html"
+
+      classfrag = (target.include? "#") ? '#' + target.gsub(/.*#/, '') : ''
+      label = (attrs.has_key? 'label') ? attrs['label'] : classname
+      target = %(http://stapler.kohsuke.org/apidocs/#{classurl}#{classfrag})
+
+      title = %(Javadoc for #{classname})
+
+      (create_anchor parent, label, type: :link, target: target, attributes: {'title' => title}).render
+    end
+  end
+end
+
+
+Asciidoctor::Extensions.register do
+  inline_macro do
+    named :javadoc
+    name_positional_attributes 'label'
+
+    process do |parent, target, attrs|
+
+      classname = target.gsub(/#.*/, '')
+      classurl = classname.gsub(/\./, '/') + ".html"
+
+      classfrag = (target.include? "#") ? '#' + target.gsub(/.*#/, '') : ''
+      label = (attrs.has_key? 'label') ? attrs['label'] : classname
+      target = %(http://docs.oracle.com/javase/7/docs/api/#{classurl}#{classfrag})
 
       title = %(Javadoc for #{classname})
 
