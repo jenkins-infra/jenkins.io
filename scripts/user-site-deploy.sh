c61b4becccce6b6571c9eaa4d40966c8bb82c89b
Liam Newman <bitwiseman@gmail.com>
2018-02-02 18:14:28 -0800

    Make site publish by branch

--- scripts/user-site-deploy.sh
+++ scripts/user-site-deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ] ; then
+  echo Missing branch name.
+  exit 1
+fi
+
+BRANCH=$1
+
 # if there is no remote gh-pages
 if git ls-remote --exit-code --heads origin gh-pages; then
   echo Found origin/gh-pages...
@@ -10,17 +17,21 @@ else
     echo Delete the local gh-pages and try deploy again.
     exit 1
   else
+    echo "Creating new gh-pages brach"
     git checkout --orphan gh-pages || exit 1
+    git rm -rf . || exit 1
   fi
 fi
 
-
-git --work-tree build/_site/ add --all . &&
-  git commit -m 'Publish user site to gh-pages' &&
+mkdir -p build/_gh-pages &&
+  cp -r build/_site build/_gh-pages/"${BRANCH}"
+  git --work-tree build/_gh-pages/ add --all -- "${BRANCH}" &&
+  git commit -m "Publish site for ${BRANCH} branch to gh-pages" &&
   git push origin gh-pages
 
 RESULT=$?
-
+rm -rf build/_gh-pages
+rm -rf "${BRANCH}"
 git reset --hard && git checkout - || exit 1
 
 exit $RESULT
