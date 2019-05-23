861bfe1fd5075501c6330bfa4f34a3cc6faed539
Zbynek Konecny <zbynek@geogebra.org>
2019-05-24 01:14:13 +0200

    [WEBSITE-64] add author pages

---
+++ content/_ext/authorlist.rb
@@ -0,0 +1,56 @@
+require 'awestruct/page'
+
+# The SolutionPage helper will set up the solution page short-links for
+# marketing purposes under /s/$foo
+#
+# The pages are equivalent
+class AuthorList
+   module AuthorLink
+     def author_link(string)
+       #replace accents with unaccented version, go lowercase and replace and space with dash
+       "/node/authors/" + string.to_s.urlize({:convert_spaces=>true})
+     end
+   end      
+   
+   include AuthorLink
+
+   def initialize(tagged_items_property, input_path, opts={})
+     @tagged_items_property = tagged_items_property
+     @input_path  = input_path
+     @pagination_opts = opts
+   end
+
+   def execute(site)
+     @authors ||= {}
+     all = site.send( @tagged_items_property )
+     return if ( all.nil? || all.empty? ) 
+     all.each do |page|
+       author = page.author
+       if ( author )
+         author = author.to_s
+         @authors[author] ||= Awestruct::Extensions::Tagger::TagStat.new( author, [] )
+         @authors[author].pages << page
+       end
+     end
+
+     ordered_authors = @authors.values
+     # secondary sort: user ID
+     ordered_authors.sort!{|l,r| l.to_s <=> r.to_s}
+     # primary sort: number of posts
+     ordered_authors.sort!{|l,r| -(l.pages.size <=> r.pages.size)}
+
+     @authors.values.each do |author|
+       output_prefix = author_link(author.to_s)
+       options = { :remove_input=>false, :output_prefix=>output_prefix, :collection=>author.pages, :selected_tag=>author.to_s }.merge( @pagination_opts )
+          
+       paginator = Awestruct::Extensions::Paginator.new( @tagged_items_property, @input_path, options )
+       primary_page = paginator.execute( site )
+       primary_page.author = author.to_s
+       author.primary_page = primary_page
+       site.send( "#{@tagged_items_property}_authors=", ordered_authors )
+       author.pages.each {|p| primary_page.dependencies.add_dependency(p)}
+     end
+
+   end
+
+end
--- content/_ext/authorship.rb
+++ content/_ext/authorship.rb
@@ -3,43 +3,23 @@
 # This only renders something if the author has put information in
 # content/_data/authors and the document has an `author` attribute in its
 # front-matter
+require 'authorlist.rb'
 module Authorship
-  # The order of preference for linking an author based on their meta-data
-  AUTHOR_LINK_PREFERENCE = [
-    [:github, 'https://github.com/VALUE'],
-    [:twitter, 'https://twitter.com/VALUE'],
-    [:blog, 'VALUE'],
-  ].freeze
+  include AuthorList::AuthorLink
 
-  def display_author_for(node, link = nil)
+  def display_author_for(node)
     # bail early if what we were given doesn't even respond
     return unless node.author
 
     author = node.author.to_sym
-
     if node.author && site.authors.has_key?(author)
       full_name = site.authors[author].name
-
-      # If the caller provided a link use it, otherwise
-      if link.nil?
-        # Let's find a nice link to give our author
-        AUTHOR_LINK_PREFERENCE.each do |link_type, url|
-          value = site.authors[author].send(link_type)
-          # If we didn't get anything, skip
-          next if value.nil?
-
-          link = url.gsub(/VALUE/, value.to_s)
-          break unless link.nil?
-        end
-
-        if link.nil?
-          return full_name
-        end
-      end
-
-      return "<a href=\"#{link}\">#{full_name}</a>"
+    else
+      full_name = author
     end
 
-    return node.author
+    link = author_link(author)
+
+    return "<a href=\"#{link}\">#{full_name}</a>"
   end
 end
