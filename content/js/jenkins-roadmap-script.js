08853045695a60b6280591528a70497e9b49b7a9
Oleg Nenashev <o.v.nenashev@gmail.com>
2020-03-09 12:30:24 +0100

    Adjust rendering to the Public Roadmap proposal, add Mock data

--- content/js/jenkins-roadmap-script.js
+++ content/js/jenkins-roadmap-script.js
@@ -9,12 +9,12 @@ function createRoadmapCategory(categoryName) {
       switch (item.status) {
         case 'not_planned':
           return 'Not planned to be delivered<br>in the next 12 months.';
-        case 'planned':
-          return 'Planned to be delivered<br>within the next 12 months.';
-        case 'in_progress':
-          return 'Design and development has started';
-        case 'ready_soon':
-          return 'Ready for release in the next month';
+        case 'current':
+          return 'Things being worked on presently with a specific scope, typically a JEP, though no specific delivery dates';
+        case 'near-term':
+          return 'We intend to work on that in the short term, but there is no ongoing development';
+        case 'future':
+          return 'There is an interest to work on that in the community, but no confirmed plans';
         case 'released':
           return 'Released';
         default:
@@ -23,16 +23,32 @@ function createRoadmapCategory(categoryName) {
     }
   }
   
-  function createRoadapItem(item) {
+  function mapStatusToStyle(status) {
+    switch (status) {
+        case 'released':
+            return 'released';
+        case 'current':
+            return "in-progress";
+        case 'near-term':
+            return 'ready-soon';
+        case 'future':
+            return 'planned';
+        default:
+            return 'unknown';
+    }
+  }
+
+  function createRoadmapItem(item) {
     var tipAttrs = 'data-delay="200" data-html="true" data-offset="2 0" data-placement="top" data-toggle="tooltip" title=""';
     var inner = '<td class="feat-name"><a href="' + item.link + '">' + item.name + '</a></td>';
-    inner += '<td class="status not-planned"><span data-original-title="' + tooltip(item) + '" ' + tipAttrs + '>Not planned</span></td>';
-    inner += '<td class="status planned"><span data-original-title="' + tooltip(item) + '" ' + tipAttrs + '>Planned</span></td>';
-    inner += '<td class="status in-progress"><span data-original-title="' + tooltip(item) + '" ' + tipAttrs + '>In Progress</span></td>';
-    inner += '<td class="status ready-soon"><span data-original-title="' + tooltip(item) + '" ' + tipAttrs + '>Ready soon</span></td>';
     inner += '<td class="status released"><span data-original-title="' + tooltip(item) + '" ' + tipAttrs + '>Released</span></td>';
-    return $('<tr class="status-' + item.status.replace('_', '-') + '">' + inner + '</tr>')
+    inner += '<td class="status in-progress"><span data-original-title="' + tooltip(item) + '" ' + tipAttrs + '>Current</span></td>';
+    inner += '<td class="status ready-soon"><span data-original-title="' + tooltip(item) + '" ' + tipAttrs + '>Near Term</span></td>';
+    inner += '<td class="status planned"><span data-original-title="' + tooltip(item) + '" ' + tipAttrs + '>Future</span></td>';
+    return $('<tr class="status-' + mapStatusToStyle(item.status) + '">' + inner + '</tr>')
   }
+
+  
   
   function buildTable(roadmap) {
     var rows = [];
@@ -42,7 +58,7 @@ function createRoadmapCategory(categoryName) {
       }
       var items = roadmap.categories[categoryName];
       for (var i = 0; i < items.length; i++) {
-        rows.push(createRoadapItem(items[i]));
+        rows.push(createRoadmapItem(items[i]));
       }
     }
     $('.roadmap-table tbody').append(rows);
@@ -55,6 +71,7 @@ function createRoadmapCategory(categoryName) {
     req.onload = function() {
       buildTable(JSON.parse(req.responseText));
     };
+    //TODO(oleg_nenashev): Use separate repo and a CDN like jsdelivr?
     req.open("GET", '/project/roadmap/data.json');
     req.send();
   }
