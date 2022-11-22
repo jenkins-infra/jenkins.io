8244e4c47fdbc8e496afcd6ff5a9b756d9a7034d
Mark Waite <mark.earl.waite@gmail.com>
2022-11-21 21:24:43 -0700

    Revert "Update Bootstrap to 5.2, replace Tether with Popper (#5436)" (#5716)
    Drop-down menus were not appearing when I clicked the button at the top of the
    page for "Documentation", "Community", "Subprojects", and "About".
    
    This reverts commit 8cfb4a85dcbd4d6e781d5f60fd5fe2f04c1ddb86.

--- content/js/roadmap.js
+++ content/js/roadmap.js
@@ -45,10 +45,3 @@ function filterRoadmap() {
     }
   }
 }
-
-window.addEventListener("load", function() {
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
-  });
-})
