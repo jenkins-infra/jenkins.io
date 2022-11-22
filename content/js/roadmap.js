c7048991fa81b673c7844831cc06fc50f8c0941e
Zbynek Konecny <zbynek1729@gmail.com>
2022-11-22 21:47:18 +0100

    Revert "Revert "Update Bootstrap to 5.2, replace Tether with Popper (#5436)" (#5716)"
    This reverts commit 8244e4c47fdbc8e496afcd6ff5a9b756d9a7034d.

--- content/js/roadmap.js
+++ content/js/roadmap.js
@@ -45,3 +45,10 @@ function filterRoadmap() {
     }
   }
 }
+
+window.addEventListener("load", function() {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+})
