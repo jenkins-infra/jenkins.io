f2b4ee8e56b47d736bc1c3c3d23d01cd67888efa
SameerGoyal125 <66504149+SameerGoyal125@users.noreply.github.com>
2023-03-23 15:51:44 +0530

    Added Copy Button

---
+++ content/js/download.js
@@ -0,0 +1,18 @@
+function copyToClipboard(event) {
+    const target = event.currentTarget.getAttribute('data-clipboard-target');
+    const text = event.currentTarget.parentNode.getAttribute('data-clipboard-text');
+    navigator.clipboard.writeText(text);
+}
+function changeIcon(iconId) {
+    var icon = document.getElementById(iconId);
+    if (icon) {
+    icon.setAttribute('name', 'checkmark-done-outline');
+    setTimeout(function() {
+    icon.setAttribute('name', 'copy-outline');
+    }, 2000); 
+    }
+}
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip({trigger: 'hover'});
+  });
