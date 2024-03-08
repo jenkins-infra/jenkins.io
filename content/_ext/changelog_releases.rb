7e104d181b0a042dcb2eabc668b618874049fa87
Daniel Beck <daniel-beck@users.noreply.github.com>
2024-03-08 10:44:45 +0100

    Implement version limiter (no changelog pages for unreleased versions)

--- content/_ext/changelog_releases.rb
+++ content/_ext/changelog_releases.rb
@@ -10,15 +10,18 @@ class ChangelogReleases
 
   def execute(site)
     changelog_entries = site.changelogs[@release_line]
+    limit_version = Gem::Version.new(site.jenkins[@limit_version_key])
 
     entry_template = Pathname.new(::Awestruct::Engine.instance.site.config.dir).join('_layouts/changelog_entry.html.haml')
 
     changelog_entries.each do |changelog_entry|
-      page = site.engine.load_page(entry_template)
-      page.output_path = "/#{@changelog_path}/#{changelog_entry.version}/index.html"
-      page.release = changelog_entry
-      page.title = page.title + " for " + changelog_entry.version
-      site.pages << page
+      if Gem::Version.new(changelog_entry.version) <= limit_version
+        page = site.engine.load_page(entry_template)
+        page.output_path = "/#{@changelog_path}/#{changelog_entry.version}/index.html"
+        page.release = changelog_entry
+        page.title = page.title + " for " + changelog_entry.version
+        site.pages << page
+      end
     end
   end
 end
